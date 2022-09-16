407da692186d882aa7576689b0a8d667ad303198
Lucy Yaneth Jaimes <yaneth18jaimes@gmail.com>
2022-09-16 09:08:51 -0500

    segundo

---
+++ Repaso.js
@@ -0,0 +1,28 @@
+function sumar(params) {
+    let suma= a + b;
+    console.log(suma)
+}
+
+const  a = 1;
+const b = 2;
+
+sumar();
+
+
+/* funcion expresada*/ 
+
+const sumar1= function(a,b){
+    let suma= a + b;
+    console.log(suma)
+}
+sumar();
+
+
+
+
+/* arrow funcion */
+const sumar= function(a,b){
+    let suma= a + b;
+    console.log(suma)
+}
+sumar();
