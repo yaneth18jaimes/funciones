6ee1a53db8c89b081e9502fd228fec211f6a1aa0
Lucy Yaneth Jaimes <yaneth18jaimes@gmail.com>
2022-09-16 09:37:51 -0500

    funcion multiplicate

--- Repaso.js
+++ Repaso.js
@@ -1,28 +1,40 @@
-function sumar(params) {
-    let suma= a + b;
-    console.log(suma)
+// function sumar(params) {
+//     let suma= a + b;
+//     console.log(suma)
+// }
+
+// const  a = 1;
+// const b = 2;
+
+// sumar();
+
+
+// /* funcion expresada*/ 
+
+// const sumar1= function(a,b){
+//     let suma= a + b;
+//     console.log(suma)
+// }
+// sumar();
+
+
+
+
+// /* arrow funcion */
+// const sumar= function(a,b){
+//     let suma= a + b;
+//     console.log(suma)
+// }
+// sumar();
+
+
+
+let number= parseInt(prompt("give me a number "))
+
+function multiplicate(number) {
+    for (let i = 1; i <= 9; i++){
+        document.write(`${number} x  ${i} = ${number*i}</br>`)      
+    }
+
 }
-
-const  a = 1;
-const b = 2;
-
-sumar();
-
-
-/* funcion expresada*/ 
-
-const sumar1= function(a,b){
-    let suma= a + b;
-    console.log(suma)
-}
-sumar();
-
-
-
-
-/* arrow funcion */
-const sumar= function(a,b){
-    let suma= a + b;
-    console.log(suma)
-}
-sumar();
+multiplicate(number)
